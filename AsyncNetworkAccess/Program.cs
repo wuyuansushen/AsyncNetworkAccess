d3fae17f44ddc5e7d6d7e18cb100e38af2f3d6af
wuyuansushen <819070864@qq.com>
2021-04-09 21:12:34 +0800

    Add project files.

---
+++ AsyncNetworkAccess/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading;
+
+namespace AsyncNetworkAccess
+{
+    class Program
+    {
+        async static Task Main()
+        {
+            var client = new HttpClient();
+            string urlUsed = "https://cn.bing.com";
+            Console.Write("Input Url for request: ");
+            string urlIn = Console.ReadLine();
+            var getTask = client.SendAsync(new HttpRequestMessage() { Method = HttpMethod.Get, RequestUri = new Uri((urlIn.Trim()).Length==0 ? urlUsed:urlIn) }) ;
+            double i;
+            for (i = 0; i < 1000; i++)
+            {
+                if (getTask.IsCompleted)
+                {
+                    Console.WriteLine($" {(await getTask).StatusCode}");
+                    i /= 100;
+                    Console.WriteLine($"Usage: {i}s");
+                    break;
+                }else
+                {
+                    Thread.Sleep(10);
+                }
+            }
+        }
+    }
+}
